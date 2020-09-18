96e600b2f3d0ced20284141629e9ed53281b8d2f
robertvict <robertvict@myvuw.ac.nz>
2020-09-18 12:40:33 +1200

    Fixed Tile class and added a variable in InfoField class

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/InfoField.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/InfoField.java
@@ -2,4 +2,6 @@ package nz.ac.vuw.ecs.swen225.gp20.maze;
 
 class InfoField extends Accessible {
 
+  String information; //information that stored in the infoField tile
+
 }
--- src/nz/ac/vuw/ecs/swen225/gp20/maze/Tile.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/Tile.java
@@ -1,4 +1,8 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze;
 
 public interface Tile {
+  public boolean isAccessible();
+  //public String giveInfo();
+  //public boolean winGame();
 }
+
