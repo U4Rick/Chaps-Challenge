7e9c5564f0494adbd75df54050c4ea934aab902a
Matt <matthillwakatipu@gmail.com>
2020-09-19 11:09:34 +1200

    Added overridden method Baboon.selectMove to check behaviour when attempting to move out of bounds.

--- test/nz/ac/vuw/ecs/swen225/gp20/monkey/Baboon.java
+++ test/nz/ac/vuw/ecs/swen225/gp20/monkey/Baboon.java
@@ -1,5 +1,11 @@
 package nz.ac.vuw.ecs.swen225.gp20.monkey;
 
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+
+import java.util.Random;
+
+import static nz.ac.vuw.ecs.swen225.gp20.maze.Maze.*;
+
 /**
  * An AI that plays based on purely random inputs.
  */
@@ -10,5 +16,11 @@ public class Baboon extends MonkeyAI {
      */
     public Baboon() {
         super(0, 0, 0, 0, 0, 0, 0);
+    }
+
+    @Override
+    public Direction selectMove(Maze maze) {
+        int number = new Random().nextInt(Direction.values().length - 1);   //Get random ordinal
+        return Direction.values()[number];                                          //Return direction from ordinal
     }
 }
