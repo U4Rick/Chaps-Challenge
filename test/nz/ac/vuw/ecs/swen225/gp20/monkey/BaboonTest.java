98c1ae8cd51b7606b74473e2a1bd4ed82d2501be
Matt <matthillwakatipu@gmail.com>
2020-09-18 23:13:32 +1200

    Split locked door reward to account for having the key or not. MonkeyAI.utilityFunction now assigns EVs based on tile type. MonkeyAI.selectMove now checks every direction for highest EV tile. Created MonkeyAI.getDestinationTile.

--- test/nz/ac/vuw/ecs/swen225/gp20/monkey/BaboonTest.java
+++ test/nz/ac/vuw/ecs/swen225/gp20/monkey/BaboonTest.java
@@ -1,5 +1,8 @@
 package nz.ac.vuw.ecs.swen225.gp20.monkey;
 
+import nz.ac.vuw.ecs.swen225.gp20.application.Main;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze.Direction;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +13,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 class BaboonTest {
 
     static MonkeyAI baboon;
+    Main main;
 
     @BeforeAll
     static void beforeAll() {
@@ -19,10 +23,19 @@ class BaboonTest {
     @BeforeEach
     void setUp() {
         //Set up new maze
+        main = new Main();
+        /*
+        main.createMaze();
+        */
     }
 
     @Test
-    void pass() {
-        assertTrue(true);
+    void exampleTest() {
+/*        for (int i = 0; i < 10_000; i++) {
+            while (main.gameIsValid()) {
+                Direction direction = baboon.selectMove(main.getMaze);
+                main.moveChap(direction);
+            }
+        }*/
     }
 }
--- test/nz/ac/vuw/ecs/swen225/gp20/monkey/MonkeyAI.java
+++ test/nz/ac/vuw/ecs/swen225/gp20/monkey/MonkeyAI.java
@@ -1,7 +1,13 @@
 package nz.ac.vuw.ecs.swen225.gp20.monkey;
 
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Tile;
+
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
+
+import static nz.ac.vuw.ecs.swen225.gp20.maze.Maze.*;
 
 /**
  * Abstract class representing the contractual requirements of MonkeyAI players.
@@ -9,25 +15,19 @@ import java.util.Random;
 public abstract class MonkeyAI {
 
     //Reward given by the utilityFunction for different types of tiles.
-    int accessibleReward;
-    int inaccessibleReward;
-    int treasureReward;
-    int keyReward;
-    int lockReward;
-    int exitReward;
+    private final int accessibleReward;
+    private final int inaccessibleReward;
+    private final int treasureReward;
+    private final int keyReward;
+    private final int lockAndKeyReward;
+    private final int lockNoKeyReward;
+    private final int exitReward;
 
     //Variance to prevent AI actions from being deterministic
-    int variance = 5;
+    static final int VARIANCE = 20;
 
     //Need to store which tiles we attempted to visit and should not revisit to not get stuck in a loop.
-    ArrayList<Object> blacklistedTiles; //FIXME use correct generic type
-
-    /**
-     * Instantiates a new MonkeyAI with default constructor.
-     */
-    public MonkeyAI() {
-
-    }
+    ArrayList<Tile> blacklistedTiles;
 
     /**
      * Instantiates a new MonkeyAI using passed reward weightings.
@@ -36,50 +36,103 @@ public abstract class MonkeyAI {
      * @param inaccessibleReward the inaccessible reward.
      * @param treasureReward     the treasure reward.
      * @param keyReward          the key reward.
-     * @param lockReward         the lock reward.
+     * @param lockAndKeyReward   the lock and key reward.
+     * @param lockNoKeyReward    the lock and no key reward.
      * @param exitReward         the exit reward.
      */
-    public MonkeyAI(int accessibleReward, int inaccessibleReward, int treasureReward, int keyReward, int lockReward, int exitReward) {
+    public MonkeyAI(int accessibleReward, int inaccessibleReward, int treasureReward, int keyReward, int lockAndKeyReward, int lockNoKeyReward, int exitReward) {
         this.accessibleReward = accessibleReward;
         this.inaccessibleReward = inaccessibleReward;
         this.treasureReward = treasureReward;
         this.keyReward = keyReward;
-        this.lockReward = lockReward;//TODO needs to check if has key or not
+        this.lockAndKeyReward = lockAndKeyReward;
+        this.lockNoKeyReward = lockNoKeyReward;
         this.exitReward = exitReward;
     }
 
     /**
-     * Assigns an numeric value to the proposed move.
+     * Assigns an numeric value to the proposed tile.
      *
-     * @param move Proposed move.
-     * @return Expected Value.
+     * @param tile Proposed Tile.
+     * @return Expected Value of Tile.
      */
-    int utilityFunction(Object move){   //TODO use more specific param type, either tile or move
+    public int utilityFunction(Tile tile){
+        int reward = 0;
 
-        Random random = new Random();
-        int v = random.nextInt(variance);
+/*        if (tile instanceof AccessibleTile) {
+            reward = accessibleReward;
+        } else if (tile instanceof InaccessibleTile) {
+            reward = inaccessibleReward;
+        } else if (tile instanceof TreasureTile) {
+            reward = treasureReward;
+        } else if (tile instanceof KeyTile) {
+            reward = keyReward;
+        } else if (tile instanceof LockedDoorTile) {
+            if (hasKey) {
+                reward = lockAndKeyReward;
+            } else {
+                reward = lockNoKeyReward;
+            }
+        } else if (tile instanceof ExitTile) {
+            reward = exitReward;
+        }*/
 
-        //switch on type of tile being moved to
+        int variance = new Random().nextInt(VARIANCE);  //Calculate variance between 0 and VARIANCE
 
-        //return reward + variance
-        return 0;
+        return reward + variance;
     }
 
     /**
      * Checks the Expected Value of each possible move and selects the best.
      *
-     * @param currentPosition Current position on the board.
+     * @param maze Maze of the current game.
      * @return Move with the highest Expected Value.
      */
-    Object selectMove(Object currentPosition) { //TODO use more specific param type
+    public Direction selectMove(Maze maze) {
+        Direction bestDirection = null;
+        int bestEV = Integer.MIN_VALUE;
+
         //For each possible move, calculate EV and select the move with the highest EV
-        Object move = new Object();
-        Object bestSoFar = move;
+        for (Direction direction : Direction.values()) {
 
-        if (utilityFunction(move) > utilityFunction(bestSoFar)) {
-            bestSoFar = move;
+            Tile tile = getDestinationTile(maze, direction);
+
+            //If tile is not null, then move is valid
+            if (tile != null) {
+
+                //Calculate EV of this tile
+                int tileEV = utilityFunction(tile);
+
+                //If tileEV is better set bestDirection and bestEV
+                if (tileEV > bestEV) {
+                    bestDirection = direction;
+                    bestEV = tileEV;
+                }
+            }
         }
+        return bestDirection;
+    }
 
-        return bestSoFar;
+    /**
+     * Returns destination tile of the move in passed Direction.
+     *
+     * @param direction Direction of move.
+     * @return Destination tile.
+     */
+    private Tile getDestinationTile(Maze maze, Direction direction) {
+        /*Point chapPosition = maze.getChapPosition();
+
+        //TODO add checks for out of bounds
+        switch (direction) {
+            case UP:
+                return maze.getTileAt(chapPosition.x, chapPosition.y - 1);
+            case DOWN:
+                return maze.getTileAt(chapPosition.x, chapPosition.y + 1);
+            case LEFT:
+                return maze.getTileAt(chapPosition.x - 1, chapPosition.y);
+            case RIGHT:
+                return maze.getTileAt(chapPosition.x + 1, chapPosition.y);
+        }*/
+        return null;
     }
 }
