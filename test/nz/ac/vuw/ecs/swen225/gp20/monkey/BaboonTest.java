c0a66666160588b0150f4f5cef272d3c5e1c0b52
Matt <matthillwakatipu@gmail.com>
2020-09-24 17:47:40 +1200

    First monkey test created

--- test/nz/ac/vuw/ecs/swen225/gp20/monkey/BaboonTest.java
+++ test/nz/ac/vuw/ecs/swen225/gp20/monkey/BaboonTest.java
@@ -1,14 +1,10 @@
 package nz.ac.vuw.ecs.swen225.gp20.monkey;
 
 import nz.ac.vuw.ecs.swen225.gp20.application.Main;
-import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze.Direction;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 class BaboonTest {
 
@@ -31,11 +27,12 @@ class BaboonTest {
 
     @Test
     void exampleTest() {
-/*        for (int i = 0; i < 10_000; i++) {
-            while (main.gameIsValid()) {
-                Direction direction = baboon.selectMove(main.getMaze);
-                main.moveChap(direction);
+        for (int i = 0; i < 10_000; i++) {
+            //TODO check game still valid somehow
+            while (true) {
+                Direction direction = baboon.selectMove(main.getMaze());
+                main.movePlayer(direction);
             }
-        }*/
+        }
     }
 }
