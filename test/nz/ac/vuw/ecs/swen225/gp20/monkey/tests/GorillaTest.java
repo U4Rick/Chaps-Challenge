416a2d5a19539b50ac894bd2bf18d03c983e5258
Matt <matthillwakatipu@gmail.com>
2020-09-28 20:42:35 +1300

    Sped up time delayed test

--- test/nz/ac/vuw/ecs/swen225/gp20/monkey/tests/GorillaTest.java
+++ test/nz/ac/vuw/ecs/swen225/gp20/monkey/tests/GorillaTest.java
@@ -36,9 +36,9 @@ public class GorillaTest {
     @Test
     void exampleTimeDelayTest() {
         //TODO check game still valid somehow
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             try {
-                TimeUnit.SECONDS.sleep(1);
+                TimeUnit.MILLISECONDS.sleep(100);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
