7dba6ed165bf343b3624da8764bab3fdd7694afa
Cherie Deng <cheriedengyiyi@gmail.com>
2020-09-28 21:37:31 +1300

    Change from row/col to x/y coordinate system.

--- src/nz/ac/vuw/ecs/swen225/gp20/render/BoardRenderer.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/BoardRenderer.java
@@ -1,6 +1,5 @@
 package nz.ac.vuw.ecs.swen225.gp20.render;
 
-// FIXME: remove un-allowed imports
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.*;
 
@@ -8,51 +7,48 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BoardRenderer extends JPanel {
-//    private final int TILE_SIZE = 50; // pixels
-    private final int DISPLAY_DIMENSION = 9; // rows/columns of viewer - should be odd so Chap can be centred
-    private final int BOARD_HEIGHT; // rows
-    private final int BOARD_WIDTH; // columns
-    private final int TILE_SIZE = 500/DISPLAY_DIMENSION; // FIXME: constant is temp
+    private final int DISPLAY_DIMENSION = 9; // Number of rows/columns shown (should be odd so Chap can be centred).
+    private final int BOARD_WIDTH;
+    private final int BOARD_HEIGHT;
+    private final int TILE_SIZE = 495/DISPLAY_DIMENSION; // FIXME: set value in constructor
     private final Maze maze;
 
-    public BoardRenderer(Maze importMaze){
-        // FIXME: display and tile size relying on each other
-        Dimension size = new Dimension(DISPLAY_DIMENSION * TILE_SIZE, DISPLAY_DIMENSION * TILE_SIZE);
-        setPreferredSize(size);
+    public BoardRenderer(Maze importMaze/*, Dimension size*/){
+        setPreferredSize(new Dimension(495, 495)); // FIXME: use parameter dimension
         setLayout(null);
         maze = importMaze;
-        BOARD_HEIGHT = maze.getBoard().length;
-        BOARD_WIDTH = maze.getBoard()[0].length;
+        BOARD_WIDTH = maze.getBoard().length;
+        BOARD_HEIGHT = maze.getBoard()[0].length;
+//        TILE_SIZE = size.width/DISPLAY_DIMENSION;
     }
 
     public void paintComponent(Graphics g){
         Tile[][] board = maze.getBoard();
 
         // Centre display around Chap.
-        int startRow = maze.getChap().getEntityPosition().y - (DISPLAY_DIMENSION-1)/2;
-        int startCol = maze.getChap().getEntityPosition().x - (DISPLAY_DIMENSION-1)/2;
+        int startX = maze.getChapPosition().x - (DISPLAY_DIMENSION-1)/2;
+        int startY = maze.getChapPosition().y - (DISPLAY_DIMENSION-1)/2;
         // Boundaries.
-        if(startRow < 0){
-            startRow = 0;
-        } else if(startRow + DISPLAY_DIMENSION > BOARD_HEIGHT){ // TODO: check maths >= or >
-            startRow = BOARD_HEIGHT - DISPLAY_DIMENSION;
+        if(startX < 0){
+            startX = 0;
+        } else if(startX + DISPLAY_DIMENSION > BOARD_WIDTH){
+            startX = BOARD_WIDTH - DISPLAY_DIMENSION;
         }
-        if(startCol < 0){
-            startCol = 0;
-        } else if(startCol + DISPLAY_DIMENSION > BOARD_WIDTH){
-            startCol = BOARD_WIDTH - DISPLAY_DIMENSION;
+        if(startY < 0){
+            startY = 0;
+        } else if(startY + DISPLAY_DIMENSION > BOARD_HEIGHT){ // TODO: check maths >= or >
+            startY = BOARD_HEIGHT - DISPLAY_DIMENSION;
         }
 
         // TODO: use for animation?
-//        int rowOffset = startRow * TILE_SIZE;
-//        int colOffset = startCol * TILE_SIZE;
+//        int xOffset = _0/1/-1_ * TILE_SIZE;
+//        int yOffset = _0/1/-1_ * TILE_SIZE;
 
-        for(int row = startRow; row < startRow + DISPLAY_DIMENSION; row++){
-            for(int col = startCol; col < startCol + DISPLAY_DIMENSION; col++){
-                Tile tile = board[col][row];
+        for(int xPos = startX; xPos < startX + DISPLAY_DIMENSION; xPos++){
+            for(int yPos = startY; yPos < startY + DISPLAY_DIMENSION; yPos++){
+                Tile tile = board[xPos][yPos];
                 Image tileIcon = tile.getIcon();
-//                g.drawImage(tileIcon, col * TILE_SIZE - colOffset, row * TILE_SIZE - rowOffset, TILE_SIZE, TILE_SIZE, null);
-                g.drawImage(tileIcon, (col - startCol) * TILE_SIZE, (row - startRow) * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                g.drawImage(tileIcon, (xPos - startX) * TILE_SIZE, (yPos - startY) * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
 
                 if(tile.isAccessible()){ // Tile can have something on it
                     AccessibleTile accessibleTile = (AccessibleTile) tile;
@@ -60,11 +56,10 @@ public class BoardRenderer extends JPanel {
                     if(accessibleTile.getEntityHere() != null){
                         // TODO: animate Chap
                         Image entityIcon = accessibleTile.getEntityHere().getIcon();
-//                        g.drawImage(entityIcon, col * TILE_SIZE - colOffset, row * TILE_SIZE - rowOffset, TILE_SIZE, TILE_SIZE, null);
-                        g.drawImage(entityIcon, (col - startCol) * TILE_SIZE, (row - startRow) * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
+                        g.drawImage(entityIcon, (xPos - startX) * TILE_SIZE, (yPos - startY) * TILE_SIZE, TILE_SIZE, TILE_SIZE, null);
                     }
                 }
-            } // end col for loop
-        } // end row for loop
+            }
+        }
     }
 }
