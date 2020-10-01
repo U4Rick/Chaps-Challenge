bf3f02ea4fd986fbb772a5d97772b59efcfcd70c
Rick <fudg3iscool@gmail.com>
2020-10-02 11:56:26 +1300

    added javadoc to Record

--- src/nz/ac/vuw/ecs/swen225/gp20/recnreplay/Record.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/recnreplay/Record.java
@@ -10,15 +10,25 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * Class to create a record and write out the players movements.
+ * @author Ricky McLean
+ */
 public class Record {
 
     public List<Maze.Direction> moves;
 
+    /**
+     * Constructs a record object.
+     */
     public Record() {
         this.moves = getMoves();
     }
 
+    /**
+     * Writes the movements made by the player out to a file.
+     * @throws IOException if file error occurs
+     */
     public void writeToFile() throws IOException {
         try {
             PrintWriter pw = new PrintWriter("json_data.txt");
@@ -54,14 +64,26 @@ public class Record {
         }
     }
 
+    /**
+     * Gets a list of moves made by the player.
+     * @return the list of moves
+     */
     public List<Maze.Direction> getMoves() {
         return moves;
     }
 
+    /**
+     * Set the list of moves made by the player.
+     * @param moves the list of moves.
+     */
     public void setMoves(List<Maze.Direction> moves) {
         this.moves = moves;
     }
 
+    /**
+     * Add a move to the current list of moves made by the player.
+     * @param dir the direction of the movement
+     */
     public void addMove(Maze.Direction dir) {
         this.moves.add(dir);
     }
