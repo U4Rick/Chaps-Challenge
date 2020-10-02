1b57c4a3245138ab3c59d9e2bf86457cc10f186e
Vic <victRobe@outlook.com>
2020-10-02 13:19:58 +1300

    Fixed issue of getItemHere() always returning null.

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/tiles/ExitTile.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/tiles/ExitTile.java
@@ -1,5 +1,6 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze.tiles;
 
+import nz.ac.vuw.ecs.swen225.gp20.maze.Item;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.AccessibleTile;
 
 /**
@@ -12,4 +13,9 @@ public class ExitTile extends AccessibleTile {
   }
 
   public String toString() { return "exit_tile"; }
+
+  @Override
+  public Item getItemHere() {
+    return null;
+  }
 }
