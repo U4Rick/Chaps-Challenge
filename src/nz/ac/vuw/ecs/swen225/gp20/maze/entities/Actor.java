fb79fb5fe6b32c1a3c30dc719f2855fd1987f0df
pheasetris <pheasetris@ecs.vuw.ac.nz>
2020-10-20 12:47:50 +1300

    Added actor

---
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/entities/Actor.java
@@ -0,0 +1,29 @@
+package nz.ac.vuw.ecs.swen225.gp20.maze.entities;
+
+import nz.ac.vuw.ecs.swen225.gp20.commons.Direction;
+
+import java.awt.*;
+
+public class Actor extends NPC {
+
+    Direction[] path;
+    int pathIndex;
+
+    public Actor() {
+        super(new Point(0, 0));
+
+    }
+
+    public Actor(Point startingPoint, Direction[] path) {
+        super(startingPoint);
+        this.path = path;
+        this.pathIndex = 0;
+    }
+
+    @Override
+    public Direction getNextMove() {
+        Direction dir = path[pathIndex];
+        pathIndex = (pathIndex + 1) % path.length;
+        return dir;
+    }
+}
