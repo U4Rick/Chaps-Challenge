104ef7a60f962388cb083f58fcbae381df069700
Tristan Phease <pheasetris@ecs.vuw.ac.nz>
2020-10-19 23:54:47 +1300

    Fixed tests and added persistence testing.
    Now uses a temporary folder so it doesn't create the file inside the
    directory.

--- test/nz/ac/vuw/ecs/swen225/gp20/persistence/LevelsTest.java
+++ test/nz/ac/vuw/ecs/swen225/gp20/persistence/LevelsTest.java
@@ -4,140 +4,141 @@ import nz.ac.vuw.ecs.swen225.gp20.commons.Colour;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
 import nz.ac.vuw.ecs.swen225.gp20.maze.entities.Chap;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.*;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.awt.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class LevelsTest {
-    @Test
-    public void Test_success_walls() {
-        File testFile = createTestFile();
-        Tile[][] board = getBoard(testFile);
+  
+  @Rule
+  public static TemporaryFolder tempFolder = new TemporaryFolder();
 
-        assertTrue(board[0][0] instanceof WallTile);
-        assertTrue(board[1][0] instanceof WallTile);
+  @Test
+  public void Test_success_walls() {
+    File testFile = createTestFile();
+    Tile[][] board = getBoard(testFile);
+
+    assertTrue(board[0][0] instanceof WallTile);
+    assertTrue(board[1][0] instanceof WallTile);
+  }
+
+  @Test
+  public void Test_success_size() {
+    File testFile = createTestFile();
+    Tile[][] board = getBoard(testFile);
+
+    assertTrue(board.length == 11);
+    assertTrue(board[0].length == 12);
+  }
+
+  @Test
+  public void Test_success_lockedDoor() {
+    File testFile = createTestFile();
+    Tile[][] board = getBoard(testFile);
+
+    assertTrue(board[4][3] instanceof DoorTile);
+    assertTrue(((DoorTile) board[4][3]).getDoorColour() == Colour.BLUE);
+  }
+
+  @Test
+  public void Test_success_keys() {
+    File testFile = createTestFile();
+    Tile[][] board = getBoard(testFile);
+
+    assertTrue(board[7][2] instanceof KeyTile);
+    assertTrue(((KeyTile) board[7][2]).getKeyColour() == Colour.BLUE);
+  }
+
+  @Test
+  public void Test_success_variousTiles() {
+    File testFile = createTestFile();
+    Tile[][] board = getBoard(testFile);
+
+    assertTrue(board[10][0] instanceof ExitTile);
+    assertTrue(board[9][0] instanceof ExitLockTile);
+    assertTrue(board[6][5] instanceof InfoTile);
+  }
+
+  @Test
+  public void Test_success_chapPos() {
+    File testFile = createTestFile();
+    Tile[][] board = getBoard(testFile);
+
+    assertTrue(board[5][5] instanceof FreeTile);
+    assertTrue(((FreeTile) board[5][5]).getEntityHere() instanceof Chap);
+
+    Maze testMaze = Levels.loadLevelFromFile(testFile);
+    assert testMaze != null;
+    assertTrue(testMaze.getChapPosition().equals(new Point(5, 5)));
+  }
+
+  @Test
+  public void Test_success_treasures() {
+    File testFile = createTestFile();
+    Tile[][] board = getBoard(testFile);
+
+    assertTrue(board[0][8] instanceof TreasureTile);
+    assertTrue(board[1][8] instanceof TreasureTile);
+  }
+
+  /**
+   *
+   * @param file
+   * @return
+   */
+  private Tile[][] getBoard(File file) {
+
+    Maze testMaze = Levels.loadLevelFromFile(file);
+
+    assert testMaze != null;
+    Tile[][] board = testMaze.getBoard();
+
+    return board;
+  }
+
+  /**
+   * Creates a test file with a level for testing
+   * 
+   * @return the file created
+   */
+  protected static File createTestFile() {
+    
+    try {
+      tempFolder.create();
+      File testFile = tempFolder.newFile("testLevel.json");
+
+      FileWriter fw = new FileWriter(testFile);
+  
+      fw.write("{" + "   \"number\": 1," + "   \"width\": 11," + "   \"height\": 12," + "   \"time\": 90,"
+          + "   \"walls\": [" + "      {\"x\": 0, \"y\": 0}," + "      {\"x\": 1, \"y\": 0}" + "   ],"
+          + "   \"locked_doors\": [{\"x\": 4, \"y\": 3, \"colour\": \"blue\"}],"
+          + "   \"keys\": [{\"x\": 7, \"y\": 2, \"colour\": \"blue\"}]," + "   \"exit\": {\"x\": 10, \"y\": 0},"
+          + "   \"starting_position\": {\"x\": 5, \"y\": 5}," + "   \"exit_lock\": {\"x\": 9, \"y\": 0},"
+          + "   \"info\": {\"x\": 6, \"y\": 5}," + "   \"info_text\": \"test info text\"," + "   \"actors\": [],"
+          + "   \"treasures\": [" + "      {\"x\": 0, \"y\": 8}," + "      {\"x\": 1, \"y\": 8}" + "   ]" + "}");
+  
+      fw.flush();
+  
+      fw.close();
+  
+      return testFile;
+    
+    } catch (IOException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
     }
-
-    @Test
-    public void Test_success_size() {
-        File testFile = createTestFile();
-        Tile[][] board = getBoard(testFile);
-
-        assertTrue(board.length == 11);
-        assertTrue(board[0].length == 12);
-    }
-
-    @Test
-    public void Test_success_lockedDoor() {
-        File testFile = createTestFile();
-        Tile[][] board = getBoard(testFile);
-
-        assertTrue(board[4][3] instanceof DoorTile);
-        assertTrue(((DoorTile)board[4][3]).getDoorColour() == Colour.BLUE);
-    }
-
-    @Test
-    public void Test_success_keys() {
-        File testFile = createTestFile();
-        Tile[][] board = getBoard(testFile);
-
-        assertTrue(board[7][2] instanceof KeyTile);
-        assertTrue(((KeyTile)board[7][2]).getKeyColour() == Colour.BLUE);
-    }
-
-    @Test
-    public void Test_success_variousTiles() {
-        File testFile = createTestFile();
-        Tile[][] board = getBoard(testFile);
-
-        assertTrue(board[10][0] instanceof ExitTile);
-        assertTrue(board[9][0] instanceof ExitLockTile);
-        assertTrue(board[6][5] instanceof InfoTile);
-    }
-
-    @Test
-    public void Test_success_chapPos() {
-        File testFile = createTestFile();
-        Tile[][] board = getBoard(testFile);
-
-        assertTrue(board[5][5] instanceof FreeTile);
-        assertTrue(((FreeTile)board[5][5]).getEntityHere() instanceof Chap);
-
-        Maze testMaze = Levels.loadLevelFromFile(testFile);
-        assert testMaze != null;
-        assertTrue(testMaze.getChapPosition().equals(new Point(5, 5)));
-    }
-
-    @Test
-    public void Test_success_treasures() {
-        File testFile = createTestFile();
-        Tile[][] board = getBoard(testFile);
-
-        assertTrue(board[0][8] instanceof TreasureTile);
-        assertTrue(board[1][8] instanceof TreasureTile);
-    }
-
-    /**
-     *
-     * @param file
-     * @return
-     */
-    private Tile[][] getBoard(File file) {
-
-        Maze testMaze = Levels.loadLevelFromFile(file);
-
-        assert testMaze != null;
-        Tile[][] board = testMaze.getBoard();
-
-        return board;
-    }
-
-    /**
-     * Creates a test file with a level for testing
-     * @return the file created
-     */
-     protected static File createTestFile() {
-        File testFile = new File("testLevel.json");
-        try {
-            FileWriter fw = new FileWriter(testFile);
-
-            fw.write("{" +
-                    "   \"number\": 1," +
-                    "   \"width\": 11," +
-                    "   \"height\": 12," +
-                    "   \"time\": 90," +
-                    "   \"walls\": [" +
-                    "      {\"x\": 0, \"y\": 0}," +
-                    "      {\"x\": 1, \"y\": 0}" +
-                    "   ]," +
-                    "   \"locked_doors\": [{\"x\": 4, \"y\": 3, \"colour\": \"blue\"}]," +
-                    "   \"keys\": [{\"x\": 7, \"y\": 2, \"colour\": \"blue\"}]," +
-                    "   \"exit\": {\"x\": 10, \"y\": 0}," +
-                    "   \"starting_position\": {\"x\": 5, \"y\": 5}," +
-                    "   \"exit_lock\": {\"x\": 9, \"y\": 0}," +
-                    "   \"info\": {\"x\": 6, \"y\": 5}," +
-                    "   \"treasures\": [" +
-                    "      {\"x\": 0, \"y\": 8}," +
-                    "      {\"x\": 1, \"y\": 8}" +
-                    "   ]" +
-                    "}");
-
-            fw.flush();
-
-            fw.close();
-
-            return testFile;
-
-        } catch (IOException e) {
-            fail();
-        }
-
-        return null;
-    }
+    
+    return null;
+  }
 }
--- test/nz/ac/vuw/ecs/swen225/gp20/persistence/PersistenceTest.java
+++ test/nz/ac/vuw/ecs/swen225/gp20/persistence/PersistenceTest.java
@@ -1,25 +1,41 @@
 package nz.ac.vuw.ecs.swen225.gp20.persistence;
 
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.FreeTile;
+
 import org.junit.jupiter.api.Test;
 
-import java.awt.*;
+import java.awt.Point;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 class PersistenceTest {
 
     @Test
-    public void Test_saveState() {
-        Maze maze = setupMaze();
+    public void Test_saveState() throws IOException {
+      File levelFile = LevelsTest.createTestFile();
+      Maze maze = Levels.loadLevelFromFile(levelFile);
+      
+      maze.getChap().setEntityPosition(new Point(5, 0));
+      
+      maze.setTimeLeft(43);
+      
+      //pick up treasure
+      maze.pickUpItem(new Point(0, 8));
+      //pick up key
+      maze.pickUpItem(new Point(7, 2));
+      //unlock door
+      maze.getChap().unlockDoor(new Point(4, 3), maze);
+      
+      File gameStateFile = LevelsTest.tempFolder.newFile("state.json");
+      Persistence.saveGameState(maze, gameStateFile);
+      
+      Maze maze2 = Persistence.loadGameState(gameStateFile, levelFile);
+        
+      assert(maze2.getBoard()[4][3] instanceof FreeTile);
+        
+        
 
-        File file = new File("test");
-        Persistence.saveGameState(maze, file);
-
-
-    }
-
-    private Maze setupMaze() {
-        File file = LevelsTest.createTestFile();
-        return Levels.loadLevelFromFile(file);
     }
 }
