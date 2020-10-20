b0ed6d50491f34f6f89efe2c5fb8113a77f52f2e
Cherie Deng <cheriedengyiyi@gmail.com>
2020-10-20 20:50:58 +1300

    Refactor sound files.

--- src/nz/ac/vuw/ecs/swen225/gp20/render/SoundRenderer.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/render/SoundRenderer.java
@@ -1,47 +1,44 @@
 package nz.ac.vuw.ecs.swen225.gp20.render;
 
+import nz.ac.vuw.ecs.swen225.gp20.commons.Moves;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineEvent;
-import javax.sound.sampled.LineListener;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
 
+/**
+ * A class to handle playing game sounds.
+ *
+ * @author Cherie
+ */
 public class SoundRenderer implements LineListener {
-    Map<String, String> filenameMap = new HashMap<>();
+    Map<Moves, String> filenameMap = new HashMap<>();
     boolean playCompleted;
 
+    /**
+     * A constructor that loads the .wav files into a map.
+     */
     public SoundRenderer(){
-        // TODO: load filenames into map, mapped to something related to when it needs to be called.
-        filenameMap.put("move", "resources/sounds/Move.wav");
-        filenameMap.put("treasure", "resources/sounds/Treasure_Pick_Up.wav");
-        filenameMap.put("unlock", "resources/sounds/Unlock_Door.wav");
-        filenameMap.put("unlocked", "resources/sounds/Exit_Unlocked.wav");
-        filenameMap.put("death", "resources/sounds/Death.wav");
+        // TODO: get sounds for KEY_PICKUP, ERROR, CHAP_WIN
+        for(Moves m : Moves.values()){
+            filenameMap.put(m, "resources/sounds/" + m.toString() + ".wav");
+        }
+        // TODO: just use string concat to set filepath in playSound() instead of map?
     }
 
-    public void play(/*String action*/){
-        // TODO: get the sound that needs to be played from map?
-//        playSound(filenameMap.get(action));
-        playSound(filenameMap.get("move"));
-//        playSound(filenameMap.get("treasure"));
-//        playSound(filenameMap.get("unlock"));
-//        playSound(filenameMap.get("unlocked"));
-//        playSound(filenameMap.get("death"));
-    }
-
-    private void playSound(String filepath) {
-        File audioFile = new File(filepath);
+    /**
+     * Play the sound correlating to the move that occurred.
+     *
+     * @param move the move that the sound is representing.
+     */
+    private void playSound(Moves move) {
+        String filepath = filenameMap.get(move);
 
         try {
+            File audioFile = new File(filepath);
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(audioFile);
             AudioFormat format = audioStream.getFormat();
             DataLine.Info info = new DataLine.Info(Clip.class, format);
@@ -53,7 +50,7 @@ public class SoundRenderer implements LineListener {
             // Wait for sound to finish playing.
             while (!playCompleted) {
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(1000); // TODO: check if this affects anything
                 } catch (InterruptedException ex) {
                     ex.printStackTrace();
                 }
@@ -61,7 +58,7 @@ public class SoundRenderer implements LineListener {
 
             clip.close();
 
-        } catch (UnsupportedAudioFileException ex) {
+        } catch (UnsupportedAudioFileException ex) { // TODO: remove sout and merge catches?
             System.out.println("The specified audio file is not supported.");
             ex.printStackTrace();
         } catch (LineUnavailableException ex) {
@@ -77,7 +74,7 @@ public class SoundRenderer implements LineListener {
     public void update(LineEvent event) {
         LineEvent.Type type = event.getType();
         if (type == LineEvent.Type.START) {
-            System.out.println("Playback started.");
+            System.out.println("Playback started."); // TODO: remove prints
         } else if (type == LineEvent.Type.STOP) {
             playCompleted = true;
             System.out.println("Playback completed.");
