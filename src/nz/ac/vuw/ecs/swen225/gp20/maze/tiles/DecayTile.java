bd0bcb68504bf8b3f52b9573211b5e5634d4a771
Vic <victRobe@outlook.com>
2020-10-22 16:58:04 +1300

    Implemented getIcon for DecayTile.

--- src/nz/ac/vuw/ecs/swen225/gp20/maze/tiles/DecayTile.java
+++ src/nz/ac/vuw/ecs/swen225/gp20/maze/tiles/DecayTile.java
@@ -6,7 +6,12 @@ import nz.ac.vuw.ecs.swen225.gp20.commons.Moves;
 import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
 import nz.ac.vuw.ecs.swen225.gp20.maze.entities.Entity;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Represents a tile that decays each time Chap walks out of it, becomes a WallTile when Chap walks over it enough times.
@@ -16,6 +21,7 @@ import java.awt.*;
 public class DecayTile extends AccessibleTile {
 
   int decayLevel; //current state of decay level of this tile, if it becomes equal to 0, it becomes a wall tile
+  private Map<Integer, Image> iconMap = new HashMap<>();
 
   /**
    * Constructor for the DecayTile object.
@@ -32,7 +38,7 @@ public class DecayTile extends AccessibleTile {
 
   @Override
   public String toString() {
-    return "decay_tile_"+decayLevel;
+    return "decay_tile_";
   }
 
   @Override
@@ -48,4 +54,21 @@ public class DecayTile extends AccessibleTile {
    * @return  The decay level of this tile.
    */
   public int getDecayLevel() { return decayLevel; }
+
+  @Override
+  public Image getIcon(){
+    if(iconMap.isEmpty()){
+      // Load icons on first call.
+      try{
+        iconMap.put(1, ImageIO.read(new File("./resources/" + toString() + "1" + ".png")));
+        iconMap.put(2, ImageIO.read(new File("./resources/" + toString() + "2" + ".png")));
+        iconMap.put(3, ImageIO.read(new File("./resources/" + toString() + "3" + ".png")));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+      return iconMap.get(1);
+    } else {
+      return iconMap.get(decayLevel);
+    }
+  }
 }
